e1312954cdf2b75c7e35db00137118a7a4e8328d
Yannick Yanga <52249136+yannick2019@users.noreply.github.com>
2024-09-19 14:37:12 +0200

    update login and register components

--- angular-client/src/app/components/register/register.component.ts
+++ angular-client/src/app/components/register/register.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
